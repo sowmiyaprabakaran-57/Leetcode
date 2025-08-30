16fd64069e8fc7e809e027d54238b00ef8c94da3
Sowmiya Prabakaran <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2025-08-30 22:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (43.25%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String lower=s.toLowerCase();
+        return lower;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(s);
+    }
+}
